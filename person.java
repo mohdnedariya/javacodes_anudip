cc141c683b8f9d21a1fda53287a589a6e56de884
Tushar090503 <tusharvaishya45@gmail.com>
2025-07-02 16:42:47 +0530

    my first commit

---
+++ person.java
@@ -0,0 +1,10 @@
+public class person {
+	
+	static String name= "mohammad";
+	static int age = 23;
+
+
+	public static void main (String[] args){
+		System.out.println("welcome " + name + " " + age);
+	}
+}
