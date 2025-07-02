d47d6d8d6707aad9790effcc42aa02c7b126b474
mohammad nedariya <139369416+mohdnedariya@users.noreply.github.com>
2025-07-02 17:51:36 +0530

    Update person.java

--- person.java
+++ person.java
@@ -3,8 +3,11 @@ public class person {
 	static String name= "mohammad";
 	static int age = 23;
 
-
-	public static void main (String[] args){
+	public static void persondetails(){
 		System.out.println("welcome " + name + " " + age);
+	
+	}
+	public static void main (String[] args){
+		persondetails();
 	}
 }
